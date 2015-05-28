b6b045c8cdbe90522565c2fa26af6d2e6119dc19
mininao <maxence_aici@yahoo.fr>
2015-05-28 20:01:52 +0200

    activity base model

--- index.js
+++ index.js
@@ -15,13 +15,17 @@ app.use(bodyParser.json());
 
 var port = process.env.PORT || 8080;        // set our port
 
+
+var mongoose   = require('mongoose');
+mongoose.connect('mongodb://localhost/calendar_test'); // connect to our database
+var Activity     = require('./app/models/activity');
 // ROUTES FOR OUR API
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-    res.json({ message: 'hooray! welcome to our api!' });   
+    res.json({ message: 'Hooray! welcome to our api!' });   
 });
 
 // more routes for our API will happen here
