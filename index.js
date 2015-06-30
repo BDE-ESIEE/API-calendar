832d266b02d3d0fe1c7d1a735f00348952a5bd2e
mininao <maxence_aici@yahoo.fr>
2015-06-30 13:07:20 +0200

    added

--- index.js
+++ index.js
@@ -23,11 +23,44 @@ var Activity     = require('./app/models/activity');
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
+// middleware to use for all requests
+router.use(function(req, res, next) {
+    // do logging
+    console.log(req.path + ' Requested.');
+	
+    next(); // make sure we go to the next routes and don't stop here
+});
+
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
     res.json({ message: 'Hooray! welcome to our api!' });   
 });
 
+router.route('/activities')
+
+    // create a bear (accessed at POST http://localhost:8080/api/bears)
+    .post(function(req, res) {
+        
+        var activity = new Activity();      // create a new instance of the Bear model
+        activity.name = req.body.name;  // set the bears name (comes from the request)
+
+        // save the bear and check for errors
+        activity.save(function(err) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Activity created!' });
+        });
+        
+    })
+    .get(function(req, res) {
+        Activity.find(function(err, activities) {
+            if (err)
+                res.send(err);
+
+            res.json(activities);
+		});
+	});
 // more routes for our API will happen here
 
 // REGISTER OUR ROUTES -------------------------------
